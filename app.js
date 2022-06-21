99db0807061c271a0c76915f43177a4ee535d3f7
DanielUsugaB <danusbet@gmail.com>
2022-06-20 20:49:15 -0500

    primer commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const publicPath = path.resolve(__dirname,"public")
+app.use(express.static(publicPath))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/home.html"))
+})
+
+/*app.listen(3000,()=>{
+    console.log("iniciando")
+}
+);*/
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.set('puerto',process.env.PORT || 3000)
+
+
+
+app.listen(app.get('puerto'), ()=>console.log(`Servidor escuchando en puerto ${app.get('puerto')}`));
